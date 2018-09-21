1f7779c3b10de75b222587127e777a08ed899dea
beepscore <support@beepscore.com>
2018-09-20 20:00:03 -0700

    In weather(date) read json file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 # create a Flask app
 app = Flask(__name__)
@@ -6,7 +7,13 @@ app = Flask(__name__)
 # establish a Flask route so that we can serve HTTP traffic on that route
 @app.route('/weather/<date>')
 def weather(date):
-    return 'some information'
+    # Additionally, we're now loading the JSON file's data into file_data
+    # every time a request is made to this endpoint
+    with open('./data/seattle-data.json', 'r') as jsonfile:
+        file_data = json.loads(jsonfile.read())
+    # We can then find the data for the requested date and send it back as json
+    return json.dumps(file_data[date])
+
 
 # Get setup so that if we call the app directly (and it isn't being imported elsewhere)
 # it will then run the app with the debug mode as True
