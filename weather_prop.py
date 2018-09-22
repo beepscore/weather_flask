11fedabaaf5979e30fa33bb4cce8569f16942fa1
beepscore <support@beepscore.com>
2018-09-21 19:22:26 -0700

    In docstring add info

--- weather_prop.py
+++ weather_prop.py
@@ -5,6 +5,17 @@ class WeatherProp(Enum):
     """
     enum reduces risk of misspelling dictionary keys
     https: // docs.python.org / 3 / library / enum.html
+
+    seattle-data.csv field names contains abbreviations. Doesn't include measurement units.
+
+    These abbreviations appear to match. Units may be different, not sure.
+    https://github.com/soodoku/get-weather-data/blob/master/zip2wd/column-names-info.txt
+
+    awnd average wind speed
+    http://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NCDC/.DAILY/.FSOD/.AWND/
+
+    snwd snow depth [mm]?
+    http://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NCDC/.DAILY/.FSOD/.SNWD/
     """
     STATION = "STATION"
     NAME = "NAME"
