8999a8b95fef8db4f93309f80819d00e22c24619
beepscore <support@beepscore.com>
2018-09-21 17:16:49 -0700

    In app.py call render_template

--- app.py
+++ app.py
@@ -1,22 +1,59 @@
-from flask import Flask
+# flask uses jinja to render templates
+# http://jinja.pocoo.org/
+from flask import Flask, render_template
 import json
+
+def weather_report(date):
+    with open('./data/seattle-data.json', 'r') as jsonfile:
+        weather_data = json.loads(jsonfile.read())
+
+    # get the data for the requested date and return it as a json string
+    return json.dumps(weather_data[date])
+
 
 # create a Flask app
 app = Flask(__name__)
 
-# establish a Flask route so that we can serve HTTP traffic on that route
+# set Flask routes so that we can serve HTTP traffic on them
+
+# / is the website root, the entry point
+@app.route('/')
+def index():
+    # render_template searches directory templates
+    return render_template('index.html')
+
+# route passes url date to weather function
+# allow for empty date
+# https://stackoverflow.com/questions/14023664/flask-url-route-route-several-urls-to-the-same-function
+@app.route('/weather/')
 @app.route('/weather/<date>')
-def weather(date):
-    # Additionally, we're now loading the JSON file's data into file_data
-    # every time a request is made to this endpoint
-    with open('./data/seattle-data.json', 'r') as jsonfile:
-        file_data = json.loads(jsonfile.read())
-    # We can then find the data for the requested date and send it back as json
-    return json.dumps(file_data[date])
+def weather(date=None):
+    if date is None:
+        return render_template('page.html', date='', weather_report='n/a')
+
+    spaced_date = ' ' + date
+    weather_string = weather_report(date)
+
+    # pass info to render_template
+    return render_template('page.html', date=spaced_date, weather_report=weather_string)
 
 
-# Get setup so that if we call the app directly (and it isn't being imported elsewhere)
-# it will then run the app with the debug mode as True
-# More info - https://docs.python.org/3/library/__main__.html
+# https://docs.python.org/3/library/__main__.html
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # ipv4
+    # '0.0.0.0' accessible to any device on the network, including localhost http://127.0.0.1
+    # for example on server machine http://0.0.0.0:5000/ may redirect to localhost http://127.0.0.1:5000
+    # https://www.digitalocean.com/community/questions/accessing-debugger-for-flask-127-0-0-1-5000
+    host = '0.0.0.0'
+
+    # alternatively could use ipv6
+    # https://stackoverflow.com/questions/21673068/dual-ipv4-and-ipv6-support-in-flask-applications#21689979
+    # host = '::'
+    # e.g. http://[::]:9092/
+
+    # flask default port is 5000
+    port = '5000'
+    # port = '9092'
+
+    app.run(debug=True, host=host, port=port)
